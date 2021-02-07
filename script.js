1f9c75a967eb25d23d2491d7f9e9a19a0b4cbce4
Jelenfi Gábor <dzsub4@gmail.com>
2021-02-07 17:22:21 +0100

    added date picker with minimum date that is current date

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const userInput = document.querySelector('#user-input');
 const createBtn = document.querySelector('.create');
 const tasks = document.querySelector('.content__tasks');
+const dueDate = document.querySelector('.due');
 
 let tasksHolder = [];
 const minChar = 6;
@@ -13,16 +14,21 @@ getLocalStorage();
 createBtn.addEventListener('click', addToDo);
 userInput.addEventListener('keyup', e => checkValidation(e));
 tasks.addEventListener('click', e => deleteTask(e));
+dueDate.setAttribute('min', getCurrentDate());
 
 function addToDo() {
   id = (Date.now() + '').slice(-10);
-  const html = `<div class="box__tasks" data-id="${id}">
+  const html = `
+    <div class="box__tasks" data-id="${id}">
+      <p class="due-string">${dueDate.value}</p>
       <button class="button delete">X</button>
       <p class="task">${userInput.value}</p>
      </div>`;
   tasks.insertAdjacentHTML('afterbegin', html);
   userInput.value = '';
-  tasksHolder.push({id, html});
+  tasksHolder.push({ id, html });
+  dueDate.value = '';
+  console.log(tasksHolder);
   setLocalStorage();
   checkValidation();
 }
@@ -57,7 +63,7 @@ function setLocalStorage() {
 
 function getLocalStorage() {
   const data = JSON.parse(localStorage.getItem('tasks'));
-  
+
   if (!data) return;
   tasksHolder = data;
   tasksHolder.forEach(element => {
@@ -72,6 +78,15 @@ function deleteTask(e) {
 
   if (!taskEl) return;
   taskEl.remove();
-  tasksHolder = tasksHolder.filter(del => del.id !== taskEl.dataset.id)
+  tasksHolder = tasksHolder.filter(del => del.id !== taskEl.dataset.id);
   setLocalStorage();
+}
+
+function getCurrentDate() {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+
+  return `${yyyy}-${mm}-${dd}`;
 }
