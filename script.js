d669584f8b0413897f34dbb28db287dab795d09b
Jelenfi Gábor <dzsub4@gmail.com>
2021-02-07 13:31:30 +0100

    delete single task with button next to them

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const userInput = document.querySelector('#user-input');
 const createBtn = document.querySelector('.create');
 const tasks = document.querySelector('.content__tasks');
+const deleteBtn = document.querySelector('.delete');
+
+const tasksHolder = [];
 
 createBtn.addEventListener('click', addToDo);
-userInput.addEventListener('keypress', function(e) {
-  if (e.keyCode === 13) {
-      e.preventDefault();
-      addToDo();
-  }
-});
+userInput.addEventListener('keypress', e => checkInput(e));
+tasks.addEventListener('click', e => deleteTask(e));
 
 function addToDo() {
-  tasks.insertAdjacentHTML('afterbegin', `<p>${userInput.value}</p>`);
+  const html = `<div class="box__tasks">
+      <button class="button delete">X</button>
+      <p class="task">${userInput.value}</p>
+     </div>`;
+  tasks.insertAdjacentHTML('afterbegin', html);
   userInput.value = '';
+  tasksHolder.push(html);
+  console.log(id);
+}
+
+function checkInput(e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    addToDo();
+  }
+}
+
+function deleteTask(e) {
+  if (!e.target.classList.contains('delete')) return
+
+  const taskEl = e.target.closest('.box__tasks');
+
+  if (!taskEl) return;
+  taskEl.remove();
 }
