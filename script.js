d62420994f147a28c8df2a059531404ba49929b0
Jelenfi Gábor <dzsub4@gmail.com>
2021-02-07 16:01:20 +0100

    storing and restoring after browser refresh

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const userInput = document.querySelector('#user-input');
 const createBtn = document.querySelector('.create');
 const tasks = document.querySelector('.content__tasks');
-const deleteBtn = document.querySelector('.delete');
 
-const tasksHolder = [];
+let tasksHolder = [];
 const minChar = 6;
+let savedTasks;
+let id = 0;
 createBtn.disabled = true;
+
+getLocalStorage();
 
 createBtn.addEventListener('click', addToDo);
 userInput.addEventListener('keyup', e => checkValidation(e));
 tasks.addEventListener('click', e => deleteTask(e));
 
 function addToDo() {
-  const html = `<div class="box__tasks">
+  id = (Date.now() + '').slice(-10);
+  const html = `<div class="box__tasks" data-id="${id}">
       <button class="button delete">X</button>
       <p class="task">${userInput.value}</p>
      </div>`;
   tasks.insertAdjacentHTML('afterbegin', html);
   userInput.value = '';
-  tasksHolder.push(html);
+  tasksHolder.push({id, html});
+  setLocalStorage();
   checkValidation();
 }
 
@@ -28,11 +33,9 @@ function checkValidation(e) {
     userInput.value === null ||
     userInput.value.length < minChar
   ) {
-    toggleValidation(true, '#dd3434')
-    createBtn.classList.add('disabled');
+    toggleValidation(true, '#dd3434');
   } else {
-    toggleValidation(false, '#1d9b51')
-    createBtn.classList.remove('disabled');
+    toggleValidation(false, '#1d9b51');
     if (e.keyCode === 13) {
       e.preventDefault();
       addToDo();
@@ -42,7 +45,24 @@ function checkValidation(e) {
 
 function toggleValidation(isDisabled, color) {
   createBtn.disabled = isDisabled;
-  userInput.style.border =`1px solid ${color}`;
+  userInput.style.border = `1px solid ${color}`;
+  // prettier-ignore
+  isDisabled ? createBtn.classList.add('disabled') : createBtn.classList.remove('disabled');
+}
+
+function setLocalStorage() {
+  localStorage.clear();
+  localStorage.setItem('tasks', JSON.stringify(tasksHolder));
+}
+
+function getLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('tasks'));
+  
+  if (!data) return;
+  tasksHolder = data;
+  tasksHolder.forEach(element => {
+    tasks.insertAdjacentHTML('afterbegin', element.html);
+  });
 }
 
 function deleteTask(e) {
@@ -52,4 +72,6 @@ function deleteTask(e) {
 
   if (!taskEl) return;
   taskEl.remove();
+  tasksHolder = tasksHolder.filter(del => del.id !== taskEl.dataset.id)
+  setLocalStorage();
 }
