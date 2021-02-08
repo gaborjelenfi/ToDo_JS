07d4cfec83b8cd781cb03006d80316e93bf659e1
Jelenfi Gábor <dzsub4@gmail.com>
2021-02-08 23:08:24 +0100

    added edit feature

--- script.js
+++ script.js
@@ -3,53 +3,90 @@ const createBtn = document.querySelector('.create');
 const tasks = document.querySelector('.content__tasks');
 const dueDate = document.querySelector('.due');
 const radioBtns = document.getElementsByName('prior');
+const form = document.getElementById('form');
 
 let tasksHolder = [];
 const minChar = 6;
 let savedTasks;
 let id = 0;
 createBtn.disabled = true;
+let editMode = false;
+let currentEl = -1;
 
 getLocalStorage();
 
-createBtn.addEventListener('click', addToDo);
-userInput.addEventListener('keyup', e => checkValidation(e));
+createBtn.addEventListener('click', e => addToDo(e));
+userInput.addEventListener('keyup', isValid);
+userInput.addEventListener('keypress', e => addWithEnter(e));
 tasks.addEventListener('click', e => deleteTask(e));
+tasks.addEventListener('click', e => editTask(e));
 dueDate.setAttribute('min', getCurrentDate());
 
-function addToDo() {
+function getFormData() {
   id = (Date.now() + '').slice(-10);
   const priority = addPriority();
   const html = `
     <div class="box__tasks" data-id="${id}">
+      <button type="button" class="button edit">edit</button>
       <p class="due-string">${dueDate.value}</p>
       <button class="button delete">X</button>
       <p class="task ${priority}">${userInput.value}</p>
-     </div>`;
-  tasks.insertAdjacentHTML('afterbegin', html);
-  userInput.value = '';
-  tasksHolder.push({ id, html });
-  dueDate.value = '';
-  console.log(tasksHolder);
-  checkValidation();
-  addPriority();
-  setLocalStorage();
-  console.log(tasksHolder);
+    </div>`;
+
+  return {
+    dueDate: dueDate.value,
+    userInputValue: userInput.value,
+    priority,
+    id,
+    html,
+  };
 }
 
-function checkValidation(e) {
+function addToDo(e) {
+  const data = getFormData();
+
+  if (e.target.classList.contains('create')) {
+    tasks.insertAdjacentHTML('afterbegin', data.html);
+    tasksHolder.push(data);
+  }
+
+  if (e.target.classList.contains('save')) {
+    const allTasks = document.querySelectorAll('.box__tasks');
+    if (currentEl < 0) return;
+    tasksHolder[currentEl] = data;
+    [...allTasks].forEach(el => el.remove());
+    showAllTasks();
+
+    createBtn.textContent = 'create';
+    createBtn.classList.remove('save');
+    createBtn.classList.add('create');
+  }
+
+  form.reset();
+  isValid();
+  addPriority();
+  setLocalStorage();
+}
+
+function isValid() {
   if (
     userInput.value === '' ||
     userInput.value === null ||
     userInput.value.length < minChar
   ) {
     toggleValidation(true, '#dd3434');
+    return false;
   } else {
     toggleValidation(false, '#1d9b51');
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      addToDo();
-    }
+    return true;
+  }
+}
+
+function addWithEnter(e) {
+  if (!isValid()) return;
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    addToDo();
   }
 }
 
@@ -68,19 +105,23 @@ function addPriority() {
   }
 }
 
-function setLocalStorage() {
-  localStorage.clear();
-  localStorage.setItem('tasks', JSON.stringify(tasksHolder));
-}
+function editTask(e) {
+  if (!e.target.classList.contains('edit')) return;
 
-function getLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('tasks'));
+  const taskEl = e.target.closest('.box__tasks');
 
-  if (!data) return;
-  tasksHolder = data;
-  tasksHolder.forEach(element => {
-    tasks.insertAdjacentHTML('afterbegin', element.html);
-  });
+  if (!taskEl) return;
+  editMode = true;
+  const [edit] = tasksHolder.filter(el => el.id === taskEl.dataset.id);
+  userInput.value = edit.userInputValue;
+  dueDate.value = edit.dueDate;
+  document.querySelector(`#${edit.priority}`).checked = true;
+  currentEl = tasksHolder.indexOf(edit);
+  createBtn.textContent = 'save';
+  createBtn.classList.remove('create');
+  createBtn.classList.add('save');
+  isValid();
+  setLocalStorage();
 }
 
 function deleteTask(e) {
@@ -94,6 +135,12 @@ function deleteTask(e) {
   setLocalStorage();
 }
 
+function showAllTasks () {
+  tasksHolder.forEach(element => {
+    tasks.insertAdjacentHTML('afterbegin', element.html);
+  });
+}
+
 function getCurrentDate() {
   let today = new Date();
   const dd = String(today.getDate()).padStart(2, '0');
@@ -101,4 +148,17 @@ function getCurrentDate() {
   const yyyy = today.getFullYear();
 
   return `${yyyy}-${mm}-${dd}`;
+}
+
+function setLocalStorage() {
+  localStorage.clear();
+  localStorage.setItem('tasks', JSON.stringify(tasksHolder));
+}
+
+function getLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('tasks'));
+
+  if (!data) return;
+  tasksHolder = data;
+  showAllTasks();
 }
